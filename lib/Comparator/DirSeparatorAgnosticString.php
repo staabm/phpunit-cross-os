6c27a39ddcd9e1e5253ec71efccb0f9d5216eca6
Markus Staab <m.staab@complex-it.de>
2021-12-12 10:06:05 +0100

    refactored logic into models

---
+++ lib/Comparator/DirSeparatorAgnosticString.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace staabm\PHPUnitCrossOs\Comparator;
+
+final class DirSeparatorAgnosticString
+{
+    /**
+     * @var string
+     */
+    private $string;
+
+    public function __construct(string $string)
+    {
+        $this->string = $string;
+    }
+
+    public function getNormalized()
+    {
+        return str_replace('\\', '/', $this->string);
+    }
+}
---
+++ lib/Comparator/EolAgnosticString.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace staabm\PHPUnitCrossOs\Comparator;
+
+final class EolAgnosticString
+{
+    /**
+     * @var string
+     */
+    private $string;
+
+    public function __construct(string $string)
+    {
+        $this->string = $string;
+    }
+
+    public function getNormalized()
+    {
+        return str_replace("\r\n", "\n", $this->string);
+    }
+}
